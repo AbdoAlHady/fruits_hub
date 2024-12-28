58841096ac8175e42ea68c167d1ae62cd4627078
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-12-28 22:12:36 +0200

    added auth repo implementation

---
+++ lib/features/auth/data/repositories/auth_repo_impl.dart
@@ -0,0 +1,13 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/error/failure.dart';
+import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repositories/auth_repo.dart';
+
+class AuthRepoImpl implements AuthRepo {
+  @override
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword(
+      {required String email, required String password}) {
+    // TODO: implement createUserWithEmailAndPassword
+    throw UnimplementedError();
+  }
+}

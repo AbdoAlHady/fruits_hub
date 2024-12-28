7047906e7293d0c7c392cd4e7b1ba0d9361066c0
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-12-28 02:21:26 +0200

    added firebase configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/app.dart';
 import 'package:fruits_hub/core/services/cache_helper.dart';
+import 'package:fruits_hub/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await CacheHelper().init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const FruitsHubApp());
 }

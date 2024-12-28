4d1ed1ca277e6a385818d275ac6a0c1bc4e55058
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-12-28 22:11:18 +0200

    - added dartZ package - added failure class to handle failure

--- lib/features/auth/domain/repositories/auth_repo.dart
+++ lib/features/auth/domain/repositories/auth_repo.dart
@@ -1,8 +1,10 @@
+import 'package:dartz/dartz.dart';
+import 'package:fruits_hub/core/error/failure.dart';
 import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
 
 abstract class AuthRepo {
   /// Sign in with email and password
-  Future<UserEntity> createUserWithEmailAndPassword({
+  Future<Either<Failure, UserEntity>> createUserWithEmailAndPassword({
     required String email,
     required String password,
   });

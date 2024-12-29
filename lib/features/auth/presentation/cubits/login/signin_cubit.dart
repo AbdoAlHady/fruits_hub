a2764d6f4367179beaab39f41c73e319d9e42794
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2024-12-29 23:49:22 +0200

    added signinWithEmailAndPassword method

--- lib/features/auth/presentation/cubits/login/signin_cubit.dart
+++ lib/features/auth/presentation/cubits/login/signin_cubit.dart
@@ -2,9 +2,22 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/enums/enums.dart';
 import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repositories/auth_repo.dart';
 
 part 'signin_state.dart';
 
 class SigninCubit extends Cubit<SigninState> {
-  SigninCubit() : super(SigninState());
+  SigninCubit(this._repo) : super(SigninState());
+  final AuthRepo _repo;
+
+  void signinWithEmailAndPassword(String email, String password) async {
+    emit(state.copyWith(stateType: StateType.loading));
+    final result = await _repo.signInWithEmailAndPassword(
+        email: email, password: password);
+    result.fold(
+        (failure) => emit(state.copyWith(
+            errorMessage: failure.message, stateType: StateType.failure)),
+        (user) =>
+            emit(state.copyWith(user: user, stateType: StateType.success)));
+  }
 }
